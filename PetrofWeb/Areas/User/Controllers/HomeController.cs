14131797d842f12c35d161492df55ccb212d088c
NIKONaaaaa <vintageblacksea@gmail.com>
2023-06-28 22:04:09 +0300

    Update

--- PetrofWeb/Areas/User/Controllers/HomeController.cs
+++ PetrofWeb/Areas/User/Controllers/HomeController.cs
@@ -1,21 +1,69 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Petrof.Models;
-using System.Diagnostics;
-
-namespace PetrofWeb.Areas.User.Controllers
+﻿namespace PetrofWeb.Areas.Customer.Controllers
 {
+    using Petrof.DataAccess.Repository.IRepository;
+    using Petrof.Models;
+    using Petrof.Models.ViewModels;
+    using Petrof.Utility;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Diagnostics;
+    using System.Security.Claims;
+
+    [Area("User")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category,ProductImages");
+            return View(productList);
+        }
+
+        public IActionResult Details(int productId)
+        {
+            ShoppingCart cart = new()
+            {
+                Product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category,ProductImages"),
+                Count = 1,
+                ProductId = productId
+            };
+            return View(cart);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult Details(ShoppingCart shoppingCart)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            shoppingCart.ApplicationUserId = userId;
+
+            ShoppingCart cartFromDatabase = _unitOfWork.ShoppingCart.Get(u => u.ApplicationUserId == userId && u.ProductId == shoppingCart.ProductId);
+
+            if (cartFromDatabase != null)
+            {
+                cartFromDatabase.Count += shoppingCart.Count;
+                _unitOfWork.ShoppingCart.Update(cartFromDatabase);
+                _unitOfWork.Save();
+            }
+            else
+            {
+                _unitOfWork.ShoppingCart.Add(shoppingCart);
+                _unitOfWork.Save();
+                HttpContext.Session.SetInt32(SD.SessionCart, _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId).Count());
+            }
+            TempData["success"] = "Cart updated successfully";
+
+            return RedirectToAction(nameof(Index));
         }
 
         public IActionResult Privacy()
