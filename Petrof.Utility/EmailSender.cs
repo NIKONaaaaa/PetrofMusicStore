deb7248335e130810383ac3c37d2f11820d90508
NIKONaaaaa <vintageblacksea@gmail.com>
2023-06-28 21:41:39 +0300

    Add project files.

---
+++ Petrof.Utility/EmailSender.cs
@@ -0,0 +1,26 @@
+﻿namespace Petrof.Utility
+{
+    using Microsoft.AspNetCore.Identity.UI.Services;
+    using Microsoft.Extensions.Configuration;
+    using SendGrid;
+    using SendGrid.Helpers.Mail;
+    using System.Threading.Tasks;
+    public class EmailSender : IEmailSender
+    {
+        //public string SendGridSecret { get; set; }
+
+        //public EmailSender(IConfiguration _config)
+        //{
+        //    _config.GetValue<string>("SendGrid:SecretKey");
+        //}
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            //var client = new SendGridClient(SendGridSecret);
+            //var from = new EmailAddress("company@mail.address", "Bulky Book");
+            //var to = new EmailAddress(email);
+            //var message = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
+            //return client.SendEmailAsync(message);
+            return Task.CompletedTask;
+        }
+    }
+}
