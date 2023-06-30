da685e38caa52b17ff2ea53df37630468bc8b151
NIKONaaaaa <vintageblacksea@gmail.com>
2023-06-30 16:37:45 +0300

    Update

---
+++ Petrof.Utility/LinkTransformer.cs
@@ -0,0 +1,14 @@
+﻿namespace Petrof.Utility
+{
+    public class LinkTransformer
+    {
+        public static string Transform(string? input)
+        {
+            if (input.Contains("youtube"))
+            {
+                return input.Replace("watch?v=", "embed/");
+            }
+            return "";
+        }
+    }
+}
