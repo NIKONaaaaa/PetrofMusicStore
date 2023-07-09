379e32f188ba5efcc1c8ba9c397fd0cadccf0699
NIKONaaaaa <vintageblacksea@gmail.com>
2023-07-09 18:40:55 +0300

    Update

---
+++ Petrof.Utility/LocalizationSetter.cs
@@ -0,0 +1,32 @@
+﻿namespace Petrof.Utility
+{
+    using Newtonsoft.Json.Linq;
+    using System.Globalization;
+    using System.Net;
+    public class LocalizationSetter
+    {
+        public string DefaultCurrency { get; set; }
+        public CultureInfo DefaultCulture { get; set; }
+
+        public const string Euro = "eur";
+        public const string AmericanDollar = "usd";
+        public const string CanadianDollar = "cad";
+        public const string AustralianDollar = "aud";
+        public const string BritishPound = "gbp";
+
+        public const string EULocal = "fr-FR";
+        public const string USALocal = "en-US";
+        public const string CANLocal = "en-CA";
+        public const string AUSLocal = "en-AU";
+        public const string UKLocal = "en-GB";
+
+        public static string GetCountryByIP()
+        {
+            string info = new WebClient().DownloadString("http://ipinfo.io");
+            var jsonObject = JObject.Parse(info);
+            var country = jsonObject.Value<string>("country");
+
+            return country;
+        }
+    }
+}
--- PetrofWeb/Areas/User/Controllers/HomeController.cs
+++ PetrofWeb/Areas/User/Controllers/HomeController.cs
@@ -24,6 +24,7 @@
 
         public IActionResult Index()
         {
+            string country = LocalizationSetter.GetCountryByIP();
             return View();
         }
 
