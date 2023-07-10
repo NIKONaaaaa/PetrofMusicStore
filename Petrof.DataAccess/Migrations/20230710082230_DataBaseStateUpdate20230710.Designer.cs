b0695dfc8e0a3b3b1de3ca0c05662354cd7e0d01
NIKONaaaaa <vintageblacksea@gmail.com>
2023-07-10 16:42:42 +0300

    Update

--- Petrof.DataAccess/Migrations/20230710082230_DataBaseStateUpdate20230710.Designer.cs
+++ Petrof.DataAccess/Migrations/20230710082230_DataBaseStateUpdate20230710.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Petrof.DataAccess.Data;
 
@@ -11,9 +12,11 @@ using Petrof.DataAccess.Data;
 namespace Petrof.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230710082230_DataBaseStateUpdate20230710")]
+    partial class DataBaseStateUpdate20230710
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
