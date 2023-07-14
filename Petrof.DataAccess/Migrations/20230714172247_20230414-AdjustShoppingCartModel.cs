448246f26080a67d7874135f0738bb053f2ace09
NIKONaaaaa <vintageblacksea@gmail.com>
2023-07-14 20:54:14 +0300

    Update

---
+++ Petrof.DataAccess/Migrations/20230714172247_20230414-AdjustShoppingCartModel.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Petrof.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class _20230414AdjustShoppingCartModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ShoppingCarts_AspNetUsers_ApplicationUserId",
+                table: "ShoppingCarts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ApplicationUserId",
+                table: "ShoppingCarts",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SessionId",
+                table: "ShoppingCarts",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ShoppingCarts_AspNetUsers_ApplicationUserId",
+                table: "ShoppingCarts",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ShoppingCarts_AspNetUsers_ApplicationUserId",
+                table: "ShoppingCarts");
+
+            migrationBuilder.DropColumn(
+                name: "SessionId",
+                table: "ShoppingCarts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ApplicationUserId",
+                table: "ShoppingCarts",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ShoppingCarts_AspNetUsers_ApplicationUserId",
+                table: "ShoppingCarts",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
