d57683f9d84ac529b9c82130daafd9c24dcf52e6
NIKONaaaaa <vintageblacksea@gmail.com>
2023-07-19 13:13:28 +0300

    Update

---
+++ Petrof.Utility/SimpleInputValidation.cs
@@ -0,0 +1,11 @@
+﻿namespace Petrof.Utility
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    public class SimpleInputValidation
+    {
+    }
+}
