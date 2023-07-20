44d26ad1124bc1d35cecec8b5dc5f619dd27ea9c
NIKONaaaaa <vintageblacksea@gmail.com>
2023-07-20 22:13:29 +0300

    Update

---
+++ Petrof.Utility/Validator.cs
@@ -0,0 +1,22 @@
+﻿namespace Petrof.Utility
+{
+    using System.Text.RegularExpressions;
+    public class Validator
+    {
+        public readonly static string Regular = @"[!@#$%^&*()\-_=+;:\'<,>./?\\\|]+";
+        public readonly static string Email = @"^[A-Za-z]+[_.-]*?[A-Za-z]+[_.-]*?[A-Za-z]+@[A-Za-z]+-?[A-Za-z]+\.[A-Za-z]+-?[A-Za-z]+(\.[A-Za-z]+-?[A-Za-z]+)?";
+        public readonly static string Number = @"^[\d]+";
+        public static bool Parse(string input, string pattern)
+        {
+            Match match = Regex.Match(input, pattern);
+            return match.Success;
+        }
+    }
+}
+//Name - Letters and spaces
+//Phone Number - Digits
+//Email Address - asd@asd.com
+//Street - Letters, spaces and digits
+//City - Letters and spaces
+//State - Letters and spaces
+//Postal Code - Digits
